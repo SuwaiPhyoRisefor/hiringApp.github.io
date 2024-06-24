54c998da07994a31d9a0197ecb4dc4d97bac80f0
Su Wai Phyo <52200194+suwaiphyo1498@users.noreply.github.com>
2024-06-24 20:57:57 +0630

    Changes service worker node

--- sw.js
+++ sw.js
@@ -14,53 +14,30 @@ self.addEventListener('install', event => {
     ]);
   })());
 });
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-});
-
-buttonInstall.addEventListener('click', (e) => {
-    // Hide the app provided install promotion
-    hideMyInstallPromotion();
-    // Show the install prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the install prompt');
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+      const cache = await caches.open(CACHE_NAME);
+  
+      // Get the resource from the cache.
+      const cachedResponse = await cache.match(event.request);
+      
+      if (cachedResponse) {
+        return cachedResponse;
       } else {
-        console.log('User dismissed the install prompt');
+          try {
+            
+            // If the resource was not in the cache, try the network.
+            const fetchResponse = await fetch(event.request);
+  
+            // Save the resource in the cache and return it.
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          } catch (e) {
+            // The network failed.
+          }
       }
-    })
+    })());
   });
 
-self.addEventListener('fetch', event => {
-  event.respondWith((async () => {
-    const cache = await caches.open(CACHE_NAME);
 
-    // Get the resource from the cache.
-    const cachedResponse = await cache.match(event.request);
-    if (cachedResponse) {
-      return cachedResponse;
-    } else {
-        try {
-          // If the resource was not in the cache, try the network.
-          const fetchResponse = await fetch(event.request);
-
-          // Save the resource in the cache and return it.
-          cache.put(event.request, fetchResponse.clone());
-          return fetchResponse;
-        } catch (e) {
-          // The network failed.
-        }
-    }
-  })());
-});
 
